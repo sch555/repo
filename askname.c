687a68cc3874fbae95e142f00a972d65d6d9643b
Scherbakov <scherbak5@mail.ru>
2014-04-24 07:02:56 -0400

    112

---
+++ askname.c
@@ -0,0 +1,9 @@
+include <stdio.h>
+#include <string.h>
+int main(int argc, char **argv)
+{
+   char first[255], last[255];
+
+   printf("Enter your names: ");
+   fgets(first, 255, stdin);
+   first[strlen(first)-1] = '\0' /* remove the newline at the end */
---
+++ askname.h
@@ -0,0 +1,5 @@
+int main(int arcg, char **argv)
+{
+   char first[255], last [255];
+   askname(first, last);
+    
