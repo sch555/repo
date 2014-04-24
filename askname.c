a749814dcfbd10063867a5fe0f332aec2863f994
Ereshko Maxim <maxereshko@gmail.com>
2014-04-24 11:05:56 +0000

    34

---
+++ askname.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+void askname(char *first, char *last)
+{
+printf("Enter your first name: ");
+fgets
+first
+}
---
+++ askname.h
@@ -0,0 +1,1 @@
+void aksname (char *first, char *last);
